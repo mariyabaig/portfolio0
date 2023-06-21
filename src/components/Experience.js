e26288a0687e2ff814cc96e95e6145f7a9688dcd
mariyabaig <mariyabaig0106@gmail.com>
2023-06-21 12:07:59 +0530

    Modified experience

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -14,20 +14,22 @@ const Experience = () => {
   };
 
   return (
-    <div className="container mx-auto p-4">
-      <h1 className="text-2xl font-bold mb-4">Mariya Baig</h1>
+    <div className="container mx-auto bg-teal text-white w-3/4 p-10 m-10">
+      {/* <h1 className="text-2xl font-bold mb-4">Mariya Baig</h1>
       <p className="mb-2">
         Indore, India • (+91) 6266929290 • mariyabaig0@gmail.com •{' '}
         <a href="https://www.linkedin.com/in/mariya-baig">linkedin.com/in/mariya-baig</a> •{' '}
         <a href="https://github.com/mariyabaig">github.com/mariyabaig</a>
-      </p>
+      </p> */}
 
       <h2 className="text-lg font-bold mt-6 mb-2">PROFESSIONAL EXPERIENCE</h2>
 
       <div className="mb-6">
         <div className="flex flex-col items-start cursor-pointer" onClick={() => toggleDetails('showDetails1')}>
-          <p className="font-bold">Associate Software Developer</p>
-        <svg
+          <p className="font-bold text-xl text-peach">Associate Software Developer</p>
+          <p className="text-peach">Nagarro, Gurgaon, India</p>
+          <span className="bg-peach text-white p-2 rounded font-bold">Aug 2022 – Present</span>
+          <svg
             xmlns="http://www.w3.org/2000/svg"
             className={`h-5 w-5 ml-1 ${dropdownState.showDetails1 ? 'transform rotate-180' : ''}`}
             viewBox="0 0 20 20"
@@ -39,14 +41,9 @@ const Experience = () => {
               clipRule="evenodd"
             />
           </svg>
-        
-          <p className="text-gray-600 font-bold">Nagarro, Gurgaon, India </p>
-          <span className='bg-darkblue text-white p-2 rounded'>Aug 2022 – Present</span>
-    
         </div>
         {dropdownState.showDetails1 && (
           <div className="ml-6 mt-2">
-           
             <ul className="list-disc list-inside ml-4">
               <li>Built and maintained a large-scale e-commerce website, optimizing performance and scalability through code optimization and the implementation of new features with minimal supervision.</li>
               <li>Collaborated with a diverse team of designers, developers, and project managers to efficiently deliver high-quality web applications within specified timeframes and budgets.</li>
@@ -57,9 +54,9 @@ const Experience = () => {
 
       <div className="mb-6">
         <div className="flex flex-col items-start cursor-pointer" onClick={() => toggleDetails('showDetails2')}>
-          <p className="font-bold">Software Developer Trainee</p>
-          <p className="text-gray-600">Nagarro, Gurgaon, India</p>
-          <span className='bg-darkblue text-white p-2 rounded'> Feb 2022 – Aug 2022</span>
+          <p className="font-bold text-xl text-peach">Software Developer Trainee</p>
+          <p className="text-peach">Nagarro, Gurgaon, India</p>
+          <span className="bg-peach text-white p-2 rounded font-bold">Feb 2022 – Aug 2022</span>
           <svg
             xmlns="http://www.w3.org/2000/svg"
             className={`h-5 w-5 ml-1 ${dropdownState.showDetails2 ? 'transform rotate-180' : ''}`}
@@ -75,7 +72,6 @@ const Experience = () => {
         </div>
         {dropdownState.showDetails2 && (
           <div className="ml-6 mt-2">
-           
             <ul className="list-disc list-inside ml-4">
               <li>Acquired knowledge and skills in software development theory and practices through training.</li>
               <li>Developed and maintained small to medium-sized web applications using React, JSX, React Router, Axios, and Material UI.</li>

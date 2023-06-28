3e13d7bf8264b0d0d8a50526d72d1833d8e91605
mariyabaig <mariyabaig0106@gmail.com>
2023-06-28 18:42:42 +0530

    Conatct form updated

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,33 +1,46 @@
 import React from "react";
-import {BiCopyright} from "react-icons/bi"
-import { FiLinkedin } from "react-icons/fi";
+import { FaEnvelope, FaLinkedin, FaWordpress, FaGithub } from "react-icons/fa";
+
 const Contact = () => {
   return (
     <>
-      <div className=" flex flex-col justify-between"
-      //</> style={{ "backgroundColor":"#7d776c","color":"#070831" }}
-      >
-        <div className="mx-16">
-          <h1 className="text-6xl bold my-7 ">Get in touch</h1>
-          <a
-            className="font-semibold px-2 text-2xl py-16 mb-6"
-            href="mailto:mariyabaig0@gmail.com"
-          >
-            mariyabaig0@gmail.com
-          </a>
-          <div className="font-semibold px-2 py-2 mt-12">
-            <span className="connect flex justify-evenly px-2 py-2"style={{ "color":"#FFF4D6" }}>
-              <a href="https://in.linkedin.com/in/mariya-baig-8a5b69217">
-             Linkedin
-              </a>
-              <a href="https://https://github.com/mariyabaig"> GitHub</a>
-              <span>+91 6266929290</span>
-            </span>
-          </div>
-          <h1 className="my-15 py-12 flex flex-row items-center px-5 "><BiCopyright/> Mariya Baig. All Rights Reserved</h1>
+      <footer className="py-4">
+        <h1 className="text-center text-3xl font-bold">mariyabaig.com</h1>
+        <div className="container flex justify-center">
+          <ul className="flex space-x-4 m-2">
+            <li>Home</li>
+            <li>About</li>
+            <li>Portfolio</li>
+            <li>Skills</li>
+            <li>Experience</li>
+            <li>Services</li>
+          </ul>
         </div>
-       
-      </div>
+        <div className="container flex justify-center">
+          <ul className="flex space-x-4">
+            <li>
+              <a href="mailto:example@example.com">
+                <FaEnvelope />
+              </a>
+            </li>
+            <li>
+              <a href="https://www.linkedin.com/">
+                <FaLinkedin />
+              </a>
+            </li>
+            <li>
+              <a href="https://wordpress.com/">
+                <FaWordpress />
+              </a>
+            </li>
+            <li>
+              <a href="https://github.com/">
+                <FaGithub />
+              </a>
+            </li>
+          </ul>
+        </div>
+      </footer>
     </>
   );
 };

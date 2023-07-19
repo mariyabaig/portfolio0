e4e83fdc5495d131235ab3f8dc1c8631ea00d635
mariyabaig <mariyabaig0106@gmail.com>
2023-07-20 01:18:55 +0530

    Styling fixed

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -7,7 +7,6 @@ const Experience = () => {
     showDetails3: false,
     showDetails4: false,
     showDetails5: false,
-    
   });
 
   const toggleDetails = (dropdownName) => {
@@ -18,154 +17,170 @@ const Experience = () => {
   };
 
   return (
-    <>
-    <div className='grid grid-cols-2'>
-    <div className="container mx-auto  w-3/4 p-10 m-10 rounded">
-
-      <h2 className="text-lg font-bold mt-6 mb-2">PROFESSIONAL EXPERIENCE</h2>
-
-      <div className="mb-6">
-        <div className="flex flex-col items-start cursor-pointer" onClick={() => toggleDetails('showDetails1')}>
-          <p className="font-bold text-xl ">Associate Software Developer</p>
-          <p className="">Nagarro, Gurgaon, India</p>
-          <span className=" text-darkblue p-1 m-1 rounded font-bold">Aug 2022 – Present</span>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className={`h-5 w-5 ml-1 ${dropdownState.showDetails1 ? 'transform rotate-180' : ''}`}
-            viewBox="0 0 20 20"
-            fill="currentColor"
+    <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
+      <div className="bg-white rounded-lg p-8 shadow-md">
+        <h2 className="text-lg font-bold mt-6 mb-2">PROFESSIONAL EXPERIENCE</h2>
+        <div className="mb-6">
+          <div
+            className="flex flex-col items-start cursor-pointer"
+            onClick={() => toggleDetails('showDetails1')}
           >
-            <path
-              fillRule="evenodd"
-              d="M6.293 7.293a1 1 0 0 1 1.414 0L10 9.586l2.293-2.293a1 1 0 1 1 1.414 1.414l-3 3a1 1 0 0 1-1.414 0l-3-3a1 1 0 0 1 0-1.414z"
-              clipRule="evenodd"
-            />
-          </svg>
-        </div>
-        {dropdownState.showDetails1 && (
-          <div className="ml-6 mt-2">
-            <ul className="list-disc list-inside ml-4">
-              <li>Built and maintained a large-scale e-commerce website, optimizing performance and scalability through code optimization and the implementation of new features with minimal supervision.</li>
-              <li>Collaborated with a diverse team of designers, developers, and project managers to efficiently deliver high-quality web applications within specified timeframes and budgets.</li>
-            </ul>
+            <p className="font-bold text-xl">Associate Software Developer</p>
+            <p className="">Nagarro, Gurgaon, India</p>
+            <span className="text-blue-600 p-1 m-1 rounded font-bold">Aug 2022 – Present</span>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className={`h-5 w-5 ml-1 ${dropdownState.showDetails1 ? 'transform rotate-180' : ''}`}
+              viewBox="0 0 20 20"
+              fill="currentColor"
+            >
+              <path
+                fillRule="evenodd"
+                d="M6.293 7.293a1 1 0 0 1 1.414 0L10 9.586l2.293-2.293a1 1 0 1 1 1.414 1.414l-3 3a1 1 0 0 1-1.414 0l-3-3a1 1 0 0 1 0-1.414z"
+                clipRule="evenodd"
+              />
+            </svg>
           </div>
-        )}
-      </div>
+          {dropdownState.showDetails1 && (
+            <div className="ml-6 mt-2">
+              <ul className="list-disc list-inside ml-4">
+                <li>
+                  Built and maintained a large-scale e-commerce website, optimizing performance and
+                  scalability through code optimization and the implementation of new features with
+                  minimal supervision.
+                </li>
+                <li>
+                  Collaborated with a diverse team of designers, developers, and project managers to
+                  efficiently deliver high-quality web applications within specified timeframes and
+                  budgets.
+                </li>
+              </ul>
+            </div>
+          )}
+        </div>
 
-      <div className="mb-6">
-        <div className="flex flex-col items-start cursor-pointer" onClick={() => toggleDetails('showDetails2')}>
-          <p className="font-bold text-xl ">Software Developer Trainee</p>
-          <p className="">Nagarro, Gurgaon, India</p>
-          <span className=" text-darkblue p-1 m-1 rounded font-bold">Feb 2022 – Aug 2022</span>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className={`h-5 w-5 ml-1 ${dropdownState.showDetails2 ? 'transform rotate-180' : ''}`}
-            viewBox="0 0 20 20"
-            fill="currentColor"
+        <div className="mb-6">
+          <div
+            className="flex flex-col items-start cursor-pointer"
+            onClick={() => toggleDetails('showDetails2')}
           >
-            <path
-              fillRule="evenodd"
-              d="M6.293 7.293a1 1 0 0 1 1.414 0L10 9.586l2.293-2.293a1 1 0 1 1 1.414 1.414l-3 3a1 1 0 0 1-1.414 0l-3-3a1 1 0 0 1 0-1.414z"
-              clipRule="evenodd"
-            />
-          </svg>
-        </div>
-        {dropdownState.showDetails2 && (
-          <div className="ml-6 mt-2">
-            <ul className="list-disc list-inside ml-4">
-              <li>Acquired knowledge and skills in software development theory and practices through training.</li>
-              <li>Developed and maintained small to medium-sized web applications using React, JSX, React Router, Axios, and Material UI.</li>
-              <li>Gained familiarity with DynamoDB, TypeScript, Jenkins.</li>
-              <li>Deployed applications on AWS using CI/CD pipelines.</li>
-              <li>Collaborated with remote team members using Git for version control, code reviews, and bug fixing.</li>
-            </ul>
+            <p className="font-bold text-xl">Software Developer Trainee</p>
+            <p className="">Nagarro, Gurgaon, India</p>
+            <span className="text-blue-600 p-1 m-1 rounded font-bold">Feb 2022 – Aug 2022</span>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className={`h-5 w-5 ml-1 ${dropdownState.showDetails2 ? 'transform rotate-180' : ''}`}
+              viewBox="0 0 20 20"
+              fill="currentColor"
+            >
+              <path
+                fillRule="evenodd"
+                d="M6.293 7.293a1 1 0 0 1 1.414 0L10 9.586l2.293-2.293a1 1 0 1 1 1.414 1.414l-3 3a1 1 0 0 1-1.414 0l-3-3a1 1 0 0 1 0-1.414z"
+                clipRule="evenodd"
+              />
+            </svg>
           </div>
-        )}
+          {dropdownState.showDetails2 && (
+            <div className="ml-6 mt-2">
+              <ul className="list-disc list-inside ml-4">
+                <li>Acquired knowledge and skills in software development theory and practices through training.</li>
+                <li>Developed and maintained small to medium-sized web applications using React, JSX, React Router, Axios, and Material UI.</li>
+                <li>Gained familiarity with DynamoDB, TypeScript, Jenkins.</li>
+                <li>Deployed applications on AWS using CI/CD pipelines.</li>
+                <li>Collaborated with remote team members using Git for version control, code reviews, and bug fixing.</li>
+              </ul>
+            </div>
+          )}
+        </div>
+      </div>
+
+      <div className="bg-white rounded-lg p-8 shadow-md">
+        <h2 className="text-lg font-bold mt-6 mb-2">EDUCATIONAL EXPERIENCE</h2>
+        <div className="mb-6">
+          <div
+            className="flex flex-col items-start cursor-pointer"
+            onClick={() => toggleDetails('showDetails3')}
+          >
+            <p className="font-bold text-xl">B.Tech (EC)</p>
+            <p className="">Acropolis Institute of Technology and Research – Indore, MP</p>
+            <span className="text-blue-600 p-1 m-1 rounded font-bold">2022</span>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className={`h-5 w-5 ml-1 ${dropdownState.showDetails3 ? 'transform rotate-180' : ''}`}
+              viewBox="0 0 20 20"
+              fill="currentColor"
+            >
+              <path
+                fillRule="evenodd"
+                d="M6.293 7.293a1 1 0 0 1 1.414 0L10 9.586l2.293-2.293a1 1 0 1 1 1.414 1.414l-3 3a1 1 0 0 1-1.414 0l-3-3a1 1 0 0 1 0-1.414z"
+                clipRule="evenodd"
+              />
+            </svg>
+          </div>
+          {dropdownState.showDetails3 && (
+            <div className="ml-6 mt-2">
+              <p className="">CGPA: 7.9</p>
+            </div>
+          )}
+        </div>
+
+        <div className="mb-6">
+          <div
+            className="flex flex-col items-start cursor-pointer"
+            onClick={() => toggleDetails('showDetails4')}
+          >
+            <p className="font-bold text-xl">12th (CBSE)</p>
+            <p className="">San Thome Academy – Dewas, MP</p>
+            <span className="text-blue-600 p-1 m-1 rounded font-bold">2018</span>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className={`h-5 w-5 ml-1 ${dropdownState.showDetails4 ? 'transform rotate-180' : ''}`}
+              viewBox="0 0 20 20"
+              fill="currentColor"
+            >
+              <path
+                fillRule="evenodd"
+                d="M6.293 7.293a1 1 0 0 1 1.414 0L10 9.586l2.293-2.293a1 1 0 1 1 1.414 1.414l-3 3a1 1 0 0 1-1.414 0l-3-3a1 1 0 0 1 0-1.414z"
+                clipRule="evenodd"
+              />
+            </svg>
+          </div>
+          {dropdownState.showDetails4 && (
+            <div className="ml-6 mt-2">
+              <p className="">CGPA: 7.0</p>
+            </div>
+          )}
+        </div>
+
+        <div className="mb-6">
+          <div
+            className="flex flex-col items-start cursor-pointer"
+            onClick={() => toggleDetails('showDetails5')}
+          >
+            <p className="font-bold text-xl">10th (CBSE)</p>
+            <p className="">San Thome Academy – Dewas, MP</p>
+            <span className="text-blue-600 p-1 m-1 rounded font-bold">2018</span>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className={`h-5 w-5 ml-1 ${dropdownState.showDetails5 ? 'transform rotate-180' : ''}`}
+              viewBox="0 0 20 20"
+              fill="currentColor"
+            >
+              <path
+                fillRule="evenodd"
+                d="M6.293 7.293a1 1 0 0 1 1.414 0L10 9.586l2.293-2.293a1 1 0 1 1 1.414 1.414l-3 3a1 1 0 0 1-1.414 0l-3-3a1 1 0 0 1 0-1.414z"
+                clipRule="evenodd"
+              />
+            </svg>
+          </div>
+          {dropdownState.showDetails5 && (
+            <div className="ml-6 mt-2">
+              <p className="">CGPA: 7.2</p>
+            </div>
+          )}
+        </div>
       </div>
     </div>
-
-    <div className="container mx-auto  w-3/4 p-10 m-10 rounded">
-  <h2 className="text-lg font-bold mt-6 mb-2">EDUCATIONAL EXPERIENCE</h2>
-
-  <div className="mb-6">
-    <div className="flex flex-col items-start cursor-pointer" onClick={() => toggleDetails('showDetails3')}>
-      <p className="font-bold text-xl ">B.Tech (EC)</p>
-      <p className="">Acropolis Institute of Technology and Research – Indore, MP</p>
-      <span className=" text-darkblue p-1 m-1 rounded font-bold">2022</span>
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        className={`h-5 w-5 ml-1 ${dropdownState.showDetails3 ? 'transform rotate-180' : ''}`}
-        viewBox="0 0 20 20"
-        fill="currentColor"
-      >
-        <path
-          fillRule="evenodd"
-          d="M6.293 7.293a1 1 0 0 1 1.414 0L10 9.586l2.293-2.293a1 1 0 1 1 1.414 1.414l-3 3a1 1 0 0 1-1.414 0l-3-3a1 1 0 0 1 0-1.414z"
-          clipRule="evenodd"
-        />
-      </svg>
-    </div>
-    {dropdownState.showDetails3 && (
-      <div className="ml-6 mt-2">
-        <p className="">CGPA: 7.9</p>
-      </div>
-    )}
-  </div>
-
-  <div className="mb-6">
-    <div className="flex flex-col items-start cursor-pointer" onClick={() => toggleDetails('showDetails4')}>
-      <p className="font-bold text-xl ">12th (CBSE)</p>
-      <p className="">San Thome Academy – Dewas, MP</p>
-      <span className=" text-darkblue p-1 m-1 rounded font-bold">2018</span>
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        className={`h-5 w-5 ml-1 ${dropdownState.showDetails4 ? 'transform rotate-180' : ''}`}
-        viewBox="0 0 20 20"
-        fill="currentColor"
-      >
-        <path
-          fillRule="evenodd"
-          d="M6.293 7.293a1 1 0 0 1 1.414 0L10 9.586l2.293-2.293a1 1 0 1 1 1.414 1.414l-3 3a1 1 0 0 1-1.414 0l-3-3a1 1 0 0 1 0-1.414z"
-          clipRule="evenodd"
-        />
-      </svg>
-    </div>
-    {dropdownState.showDetails4 && (
-      <div className="ml-6 mt-2">
-        <p className="">CGPA: 7.0</p>
-      </div>
-    )}
-  </div>
-
-  <div className="mb-6">
-    <div className="flex flex-col items-start cursor-pointer" onClick={() => toggleDetails('showDetails5')}>
-      <p className="font-bold text-xl ">10th (CBSE)</p>
-      <p className="">San Thome Academy – Dewas, MP</p>
-      <span className=" text-darkblue p-1 m-1 rounded font-bold">2018</span>
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        className={`h-5 w-5 ml-1 ${dropdownState.showDetails5 ? 'transform rotate-180' : ''}`}
-        viewBox="0 0 20 20"
-        fill="currentColor"
-      >
-        <path
-          fillRule="evenodd"
-          d="M6.293 7.293a1 1 0 0 1 1.414 0L10 9.586l2.293-2.293a1 1 0 1 1 1.414 1.414l-3 3a1 1 0 0 1-1.414 0l-3-3a1 1 0 0 1 0-1.414z"
-          clipRule="evenodd"
-        />
-      </svg>
-    </div>
-    {dropdownState.showDetails5 && (
-      <div className="ml-6 mt-2">
-        <p className="">CGPA: 7.2</p>
-      </div>
-    )}
-  </div>
-</div>
-
-</div> 
-    
-    </>
   );
 };
 

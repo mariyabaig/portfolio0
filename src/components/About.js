940b5acebe31035eaf40edc7f0341f4b323fccea
mariyabaig <mariyabaig0106@gmail.com>
2023-07-25 20:17:29 +0530

    cleaned up code

--- src/components/About.js
+++ src/components/About.js
@@ -1,66 +1,30 @@
 import React, { useState, useEffect } from "react";
-import { useNavigate,Link } from "react-router-dom";
-import Experience from "./Experience";
+
 import i1 from "../assets/i1.jpeg";
 import i2 from "../assets/i2.jpeg";
 import i3 from "../assets/i3.jpeg";
 
 import transition from "../transition";
-import Skills from "./Skills";
-import Card from "./Card";
+
 import Aboutcard from "./Aboutcard";
 
-
 const About = () => {
-  const navigate = useNavigate();
-  // const [isExpanded, setIsExpanded] = useState(false);
-  // const [showExperience, setShowExperience] = useState(false);
-
-  // const toggleExpanded = () => {
-  //   setIsExpanded(!isExpanded);
-  // };
-
-  // useEffect(() => {
-  //   if (isExpanded) {
-  //     setTimeout(() => {
-  //       setShowExperience(true);
-  //     }, 500);
-  //   } else {
-  //     setShowExperience(false);
-  //   }
-  // }, [isExpanded]);
-
   return (
     <>
       <div className="flex flex-col pt-24 h-screen px-14">
-      
-        {/* <div className="py-20">
-          <h1>
-         
-            <div className="pb-2">
-              <h1>
-              <span className="about">about</span>
-                <span className="mariya font-borel"></span>
-                 Mar
-                <span className="italic">i</span>ya.
-                <span className="span"></span>
-              </h1>
-            </div>
-          </h1>
-        </div> */}
-        <div
-              style={{ fontSize: "3vw" }} className="pt-16"
+        <div style={{ fontSize: "3vw" }} className="pt-16">
+          <h1 className="mariya font-borel">
+            <span
+              className="about font-inco text-red font-bold"
+              style={{ fontSize: "3rem" }}
             >
-            
-              <h1 className="mariya font-borel">
-              <span className="about font-inco text-red font-bold" style={{fontSize:"3rem"}}>about</span> 
-                Mar
-                <button className="italic text-red" >
-                  i
-                </button>
-                ya.
-              </h1>
-            </div>
+              about
+            </span>
+            Mar
+            <button className="italic text-red">i</button>
+            ya.
+          </h1>
+        </div>
 
         <div className="image-container flex justify-center items-center">
           <img className="about-image h-72" src={i3} alt=""></img>
@@ -70,16 +34,15 @@ const About = () => {
 
         <div className="px-32 md:mx-0 font-spartan">
           <p className="text-lg md:text-xl leading-8 md:leading-10 mt-8 md:mt-12 ">
-            Creating stunning and intuitive web experiences
-            is a true passion of mine. From ideation to execution, I thrive on
-            the process of bringing designs to life and crafting user-friendly
-            interfaces that delight users. With a keen eye for detail and a
-            commitment to excellence, I relish the opportunity to push the
-            boundaries of what's possible and exceed expectations with every
-            project. Whether it's experimenting with cutting-edge design
-            techniques or collaborating with cross-functional teams, I'm always
-            eager to explore new possibilities and take web design to the next
-            level.
+            Creating stunning and intuitive web experiences is a true passion of
+            mine. From ideation to execution, I thrive on the process of
+            bringing designs to life and crafting user-friendly interfaces that
+            delight users. With a keen eye for detail and a commitment to
+            excellence, I relish the opportunity to push the boundaries of
+            what's possible and exceed expectations with every project. Whether
+            it's experimenting with cutting-edge design techniques or
+            collaborating with cross-functional teams, I'm always eager to
+            explore new possibilities and take web design to the next level.
           </p>
           <br />
           <p className="text-lg md:text-xl leading-8 md:leading-10 mt-4 md:mt-8 ">
@@ -92,19 +55,11 @@ const About = () => {
             cycling, I find that spending time in nature helps me stay centered
             and focused. I'm also an avid reader and love diving into books on a
             wide range of topics, from technology and philosophy to history and
-            science. 
+            science.
           </p>
-          <Aboutcard/>
-
-        
-        
-         
-       
-         
+          <Aboutcard />
         </div>
       </div>
-   
-     
     </>
   );
 };

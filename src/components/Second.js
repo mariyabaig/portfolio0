df5759531b6f247af173f2ce08c0f1397c93866c
mariyabaig <mariyabaig0106@gmail.com>
2023-07-25 19:18:16 +0530

    heading aligned

--- src/components/Second.js
+++ src/components/Second.js
@@ -32,11 +32,12 @@ const Second = () => {
   ];
 
   return (
-    <div className="bg-gray-200 min-h-screen py-12">
-      <div className="container mx-auto px-4">
-      <h2 className="text-2xl font-bold mb-4 text-center">
+    <div className="bg-gray-200 min-h-screen py-12 flex flex-col">
+       <h2 className="text-2xl font-bold mb-4 text-center">
           Things I Have Written About
         </h2>
+      <div className="container mx-auto px-4">
+   
         <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
           {notes.map((note, index) => (
             <Draggable key={index}>

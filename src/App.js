dfac6a29bcc46c8a55a4f1b2ab0f07471de2aab5
mariyabaig <mariyabaig0106@gmail.com>
2023-07-27 23:47:34 +0530

    Cleaned code

--- src/App.js
+++ src/App.js
@@ -1,41 +1,5 @@
-// import React from 'react';
-// import { BrowserRouter, Route, Routes,useLocation } from 'react-router-dom';
-// import './App.css';
-// import About from './components/About';
-// import Contact from './components/Contact';
-// import Experience from './components/Experience';
-// import Home from './components/Home';
-// import Projects from './components/Projects';
-// import Navbar from './components/Navbar';
-// import Perfect from './components/Perfect';
-// import Skills from './components/Skills';
-// import { AnimatePresence } from 'framer-motion';
 
-// function App() {
-//    const location = useLocation()
-//   return (
-//     <>
-     
-//         <Navbar/>
-//         <AnimatePresence mode='wait'>
-//         <Routes location={location} key={location.pathname}>
-//         <Route index element={<Home />} />
-        
-           
-//           <Route exact path="/about" element={<About />} />
-//           <Route exact path="/experience" element={<Experience />} />
-//           <Route exact path="/services" element={<Perfect />} />
-//           <Route exact path="/projects" element={<Projects />} />
-//           <Route exact path="/contact" element={<Contact />}></Route>
-//           <Route exact path="/skills" element={<Skills />}></Route>
-//         </Routes>
-//         </AnimatePresence>
 
-//     </>
-//   );
-// }
-
-// export default App;
 import React from "react";
 import { BrowserRouter, Route, Routes, useLocation } from "react-router-dom";
 import "./App.css";
@@ -56,7 +20,7 @@ function App() {
   return (
     <>
       <Navbar />
-      {/* Wrap the Home component with the Transition component */}
+    
       <AnimatePresence mode="wait">
        
           <Routes location={location} key={location.pathname}>
